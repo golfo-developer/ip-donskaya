ccdedbdde01f4f98b400fc3271c56fd70591ec63
golfo-developer <102090985+golfo-developer@users.noreply.github.com>
2025-12-03 09:14:09 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+// ⚠️ ВАЖНО: Этот файл НЕ должен загружаться на GitHub!
+// Добавьте его в .gitignore
+
+export const CONFIG = {
+    SUPABASE_URL: 'https://ylhhaswthxeyvnfhbzff.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsaGhhc3d0aHhleXZuZmhiemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NjAyMDYsImV4cCI6MjA4MDIzNjIwNn0.scARH5fnfFJdlYeF8KBR6CLXc4xvY55zNgUUO8EOmZI',
+    VK_APP_ID: 54372400
+};
